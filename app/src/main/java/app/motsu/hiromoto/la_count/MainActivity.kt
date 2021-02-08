f8c5c43c369711b7117f6ba845f15883d92edc92
Motsu0417 <pippi9400@gmail.com>
2021-02-08 12:02:12 +0900

    Initial commit

---
+++ app/src/main/java/app/motsu/hiromoto/la_count/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.motsu.hiromoto.la_count
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    val number: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun plus(view: View){
+
+    }
+
+    fun minus(view: View){
+
+    }
+
+    fun clear(view :View){
+
+    }
+
+}
